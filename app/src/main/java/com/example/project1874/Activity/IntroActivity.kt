5e8c29b2e7d431481be8e54413ac4bc64cf6467c
adamasheel <156421001+adamasheel@users.noreply.github.com>
2024-06-10 18:30:16 +0400

    Store app

---
+++ app/src/main/java/com/example/project1874/Activity/IntroActivity.kt
@@ -0,0 +1,20 @@
+package com.example.project1874.Activity
+
+import android.content.Intent
+import android.os.Bundle
+import com.example.project1874.databinding.ActivityIntroBinding
+
+class IntroActivity : BaseActivity() {
+    private lateinit var binding: ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
